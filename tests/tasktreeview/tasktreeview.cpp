525fc661b209d7b81380fcc712974ad82817d88c
liuguangzhao <liuguangzhao@users.sf.net>
2014-04-11 19:13:58 +0800

    little test

--- tests/tasktreeview/tasktreeview.cpp
+++ tests/tasktreeview/tasktreeview.cpp
@@ -1,4 +1,6 @@
 
+#include "sqlitetaskmodel.h"
+
 #include "tasktreeview.h"
 #include "ui_tasktreeview.h"
 
@@ -6,6 +8,9 @@ TaskTreeView::TaskTreeView(QWidget *parent)
 {
     mwin = new Ui::TaskTreeView;
     mwin->setupUi(this);
+
+    SqliteTaskModel *model = SqliteTaskModel::instance(ng::cats::downloading);
+    this->mwin->treeView->setModel(model);
 }
 
 TaskTreeView::~TaskTreeView()
